14c68445b9de1582893dedc514c08113768eb9b6
r-scott25 <rachelbernal2@gmail.com>
2022-10-18 15:42:29 -0500

    added hero section to homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className="header">
         <Link to="/">
-            <h1>Portfolio</h1>
+            <h1>Portfolio.</h1>
         </Link>
         <ul className={click ? "nav-menu active" : 
     "nav-menu"}>
