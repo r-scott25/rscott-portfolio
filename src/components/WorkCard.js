51f522b63fc0c6b4a6546fb73bfab78123a2e811
r-scott25 <rachelbernal2@gmail.com>
2022-10-20 14:11:17 -0500

    created dynamic project cards

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,28 +1,26 @@
 import "./WorkCardStyles.css";
-import pro1 from "../assets/project1.png"
+
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-import React from 'react'
-
-const WorkCard = () => {
+const WorkCard = (props) => {
   return (
-    <div className="work-container">
-        <h1 className="project-heading">Projects</h1>
-        <div className="project-container">
-            <div className="project-card">
-                <img src={pro1} alt="image"/>
-                <h2 className="project-title">Adventure Outdoors</h2>
-                <div className="pro-details"></div>
-                <p>Adventure Outdoors is an app that can be used by outdoor enthusiasts who want to rent gear for their next adventure. </p>
-               
-           <div className="pro-btns">
-            <NavLink to="url.com" className="btn">View</NavLink>
-            <NavLink to="https://github.com/Jul-2022-LC-LiftOff/Adventureland" className="btn">Source</NavLink>
-           </div>
-            </div>
+    <div className="project-card">
+      <img src={props.imgsrc} alt="image" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="pro-details">
+        <p>{props.text}</p>
+        <div className="pro-btns">
+          <NavLink to={props.view} className="btn">
+            View
+          </NavLink>
+          <NavLink to={props.source} className="btn">
+            Source
+          </NavLink>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkCard
+export default WorkCard;
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+const ProjectCardData = [
+    {
+       imgsrc: pro1, 
+       title: "Adventure Outdoors",
+       text: "Adventure Outdoors is an outdoor gear rental website that my group and I created as a capstone project while enrolled in LaunchCode's Liftoff program.",
+       view: "url.com",
+       source: "https://github.com/Jul-2022-LC-LiftOff/Adventureland",
+    },
+    {
+        imgsrc: pro2, 
+        title: "Project 2 Title",
+        text: "This is my second project.",
+        view: "url.com",
+        source: "url.com",
+     },
+     {
+        imgsrc: pro3, 
+        title: "Project 3 Title",
+        text: "This is my third project.",
+        view: "url.com",
+        source: "url.com",
+     },
+]
+
+export default ProjectCardData;
