ce97af90fe069ed1fa8aea917ba48bedb4d705ef
r-scott25 <rachelbernal2@gmail.com>
2022-10-20 15:59:20 -0500

    changed photo on project card

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,29 +1,30 @@
 import pro1 from "../assets/project1.png";
 import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
+import AOHome from "../assets/AOHome.png";
 
 const ProjectCardData = [
     {
-       imgsrc: pro1, 
+       imgsrc: AOHome, 
        title: "Adventure Outdoors",
        text: "Adventure Outdoors is an outdoor gear rental website that my group and I created as a capstone project while enrolled in LaunchCode's Liftoff program.",
        view: "url.com",
        source: "https://github.com/r-scott25/Adventureland",
     },
-    {
-        imgsrc: pro2, 
-        title: "Project 2 Title",
-        text: "This is my second project.",
-        view: "url.com",
-        source: "url.com",
-     },
-     {
-        imgsrc: pro3, 
-        title: "Project 3 Title",
-        text: "This is my third project.",
-        view: "url.com",
-        source: "url.com",
-     },
+   //  {
+   //      imgsrc: pro2, 
+   //      title: "Project 2 Title",
+   //      text: "This is my second project.",
+   //      view: "url.com",
+   //      source: "url.com",
+   //   },
+   //   {
+   //      imgsrc: pro3, 
+   //      title: "Project 3 Title",
+   //      text: "This is my third project.",
+   //      view: "url.com",
+   //      source: "url.com",
+   //   },
 ]
 
 export default ProjectCardData;
